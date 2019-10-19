823b6f9313cd66c2c8b18af52f4be07c1e999fa8
abitnav <abhinav98jha@gmail.com>
2019-10-19 15:44:19 +0530

    Pre Processing

---
+++ ML/preprocessing.py
@@ -0,0 +1,80 @@
+import matplotlib
+import tensorflow as tf
+from keras.preprocessing.image import ImageDataGenerator
+from keras.layers.pooling import AveragePooling2D
+from keras.applications import ResNet50
+from keras.layers.core import Dropout
+from keras.layers.core import Flatten
+from keras.layers.core import Dense
+from keras.layers import Input
+from keras.models import Model
+from keras.optimizers import SGD
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from imutils import paths
+import matplotlib.pyplot as plt
+import numpy as np
+import argparse
+import pickle
+import cv2
+import os
+from sklearn.metrics import confusion_matrix
+from sklearn.utils.multiclass import unique_labels
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-d", "--dataset", required=True,
+	help="path to input dataset")
+ap.add_argument("-m", "--model", required=True,
+	help="path to output serialized model")
+ap.add_argument("-l", "--label-bin", required=True,
+	help="path to output label binarizer")
+ap.add_argument("-e", "--epochs", type=int, default=20,
+	help="# of epochs to train our network for")
+ap.add_argument("-p", "--plot", type=str, default="plot.png",
+	help="path to output loss/accuracy plot")
+args = vars(ap.parse_args('-d dataset -m model -l label-bin'.split()))
+
+LABELS = set(["Accident", "Fire", "Flood", "Robbery"])
+
+print("[INFO] loading images...")
+imagePaths = list(paths.list_images(args["dataset"]))
+data = []
+labels = []
+
+for imagePath in imagePaths:
+	label = imagePath.split(os.path.sep)[-2]
+
+	if label not in LABELS:
+		continue
+
+	image = cv2.imread(imagePath)
+	image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+	image = cv2.resize(image, (224, 224))
+
+	data.append(image)
+	labels.append(label)
+
+data = np.array(data)
+labels = np.array(labels)
+
+lb = LabelBinarizer()
+labels = lb.fit_transform(labels)
+
+(trainX, testX, trainY, testY) = train_test_split(data, labels,
+	test_size=0.20, stratify=labels, random_state=42)
+
+trainAug = ImageDataGenerator(
+	rotation_range=30,
+	zoom_range=0.15,
+	width_shift_range=0.2,
+	height_shift_range=0.2,
+	shear_range=0.15,
+	horizontal_flip=True,
+	fill_mode="nearest")
+
+valAug = ImageDataGenerator()
+
+mean = np.array([123.68, 116.779, 103.939], dtype="float32")
+trainAug.mean = mean
+valAug.mean = mean
